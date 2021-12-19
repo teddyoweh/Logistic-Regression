23b015e4afa2d1221d52b34a79b2d413defd3c1b
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2021-12-19 08:06:02 -1100

    Create l1_regularization.py

---
+++ l1_regularization.py
@@ -0,0 +1,62 @@
+ 
+from __future__ import print_function, division
+from builtins import range
+# Note: you may need to update your version of future
+# sudo pip install -U future
+
+
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+def sigmoid(z):
+  return 1/(1 + np.exp(-z))
+
+N = 50
+D = 50
+
+# uniformly distributed numbers between -5, +5
+X = (np.random.random((N, D)) - 0.5)*10
+# X = (np.random.randn(N, D) - 0.5)*10
+
+# true weights - only the first 3 dimensions of X affect Y
+true_w = np.array([1, 0.5, -0.5] + [0]*(D - 3))
+
+# generate Y - add noise with variance 0.5
+Y = np.round(sigmoid(X.dot(true_w) + np.random.randn(N)*0.5))
+
+
+
+
+# let's plot the data to see what it looks like
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X[:,0], X[:,1], X[:,2], c=Y)
+plt.show()
+
+# perform gradient descent to find w
+costs = [] # keep track of squared error cost
+w = np.random.randn(D) / np.sqrt(D) # randomly initialize w
+learning_rate = 0.001
+l1 = 3.0 # try different values - what effect does it have on w?
+for t in range(5000):
+  # update w
+  Yhat = sigmoid(X.dot(w))
+  delta = Yhat - Y
+  w = w - learning_rate*(X.T.dot(delta) + l1*np.sign(w))
+
+  # find and store the cost
+  cost = -(Y*np.log(Yhat) + (1-Y)*np.log(1 - Yhat)).mean() + l1*np.abs(w).mean()
+  costs.append(cost)
+
+# plot the costs
+plt.plot(costs)
+plt.show()
+
+print("final w:", w)
+
+# plot our w vs true w
+plt.plot(true_w, label='true w')
+plt.plot(w, label='w_map')
+plt.legend()
+plt.show()
